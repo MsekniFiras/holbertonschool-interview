87c0ffb587542f5eb3389ffaebbd2bc85900f3c5
MsekniFiras <msekni.firas@gmail.com>
2024-07-05 20:13:16 +0100

    testt

--- log_parsing/0-stats.py
+++ log_parsing/0-stats.py
@@ -1,39 +1,42 @@
 #!/usr/bin/python3
 """
-Script that reads stdin line by line and computes metrics
+script that reads stdin line by line and computes metrics
 """
 import sys
 
-total_size = 0
+size = 0
 counter = 0
-
-dict_codes_counter = {'200': 0, '301': 0, '400': 0, '401': 0,
-                      '403': 0, '404': 0, '405': 0, '500': 0}
-
+status_code = {
+    "200": 0,
+    "301": 0,
+    "400": 0,
+    "401": 0,
+    "403": 0,
+    "404": 0,
+    "405": 0,
+    "500": 0
+}
 try:
     for line in sys.stdin:
-        line_list = line.split(" ")
-        if len(line_list) > 2:
-            code = line_list[-2]
-            size = int(line_list[-1])
-            if code in dict_codes_counter.keys():
-                dict_codes_counter[code] += 1
-            total_size += size
+        items = line.split(" ")
+        if len(items) > 2:
+            code = items[-2]
+            size += int(items[-1])
+            if code in status_code:
+                status_code[code] += 1
             counter += 1
-
         if counter == 10:
-            print("File size: {:d}".format(total_size))
-            for k, v in sorted(dict_codes_counter.items()):
-                if v != 0:
-                    print("{}: {:d}".format(k, v))
+            print("File size: {}".format(size))
+            for status in sorted(status_code.keys()):
+                if status_code[status]:
+                    print("{}: {}".format(status, status_code[status]))
             counter = 0
 
-except Exception:
+except KeyboardInterrupt:
     pass
 
 finally:
-    print("File size: {}".format(total_size))
-    for k, v in sorted(dict_codes_counter.items()):
-        if v != 0:
-            print("{}: {}".format(k, v))
-            
+    print("File size: {}".format(size))
+    for status in sorted(status_code.keys()):
+        if status_code[status]:
+            print("{}: {}".format(status, status_code[status]))
