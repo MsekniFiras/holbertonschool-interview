02f712cf8976c2b308d86a18499478010fb6013d
MsekniFiras <msekni.firas@gmail.com>
2024-07-05 20:59:36 +0100

    test

---
+++ linked_list_cycle/0-check_cycle.c
@@ -0,0 +1,23 @@
+#include "lists.h"
+
+/**
+ * check_cycle - checks if a singly linked list has a cycle in it
+ * @list: pointer to the head of the list
+ * Return: 0 if there is no cycle, 1 if there is a cycle
+ */
+int check_cycle(listint_t *list)
+{
+    listint_t *slow = list;
+    listint_t *fast = list;
+
+    while (fast != NULL && fast->next != NULL) {
+        slow = slow->next; // Move slow pointer one step
+        fast = fast->next->next; // Move fast pointer two steps
+
+        if (slow == fast) {
+            return 1; // Cycle detected
+        }
+    }
+
+    return 0; // No cycle found
+}
---
+++ linked_list_cycle/README.md
@@ -0,0 +1,2 @@
+a function in C that checks if a singly linked list has a cycle in it.
+
