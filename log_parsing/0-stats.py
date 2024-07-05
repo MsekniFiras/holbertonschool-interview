b5cb8d39dcc791f7b76b42c4f20cd1b5be3dba2b
MsekniFiras <msekni.firas@gmail.com>
2024-07-05 19:36:52 +0100

    test

--- log_parsing/0-stats.py
+++ log_parsing/0-stats.py
@@ -1,28 +1,37 @@
 #!/usr/bin/python3
-""" Log parsing """
+"""
+Script that reads stdin line by line and computes metrics
+"""
 import sys
 
-status_codes = ["200", "301", "400", "401", "403", "404", "405", "500"]
-status_counts = {code: 0 for code in status_codes}
 total_size = 0
-line_count = 0
+counter = 0
+
+dict_codes_counter = {'200': 0, '301': 0, '400': 0, '401': 0,
+                      '403': 0, '404': 0, '405': 0, '500': 0}
+
+def print_stats():
+    print("File size: {}".format(total_size))
+    for k, v in sorted(dict_codes_counter.items()):
+        if v != 0:
+            print("{}: {}".format(k, v))
 
 try:
     for line in sys.stdin:
         parts = line.split()
-        if len(parts) > 2 and parts[-2] in status_codes:
-            status_counts[parts[-2]] += 1
+        if len(parts) > 2 and parts[-2] in dict_codes_counter:
+            dict_codes_counter[parts[-2]] += 1
             total_size += int(parts[-1])
-        line_count += 1
-        if line_count % 10 == 0:
-            print("File size: {}".format(total_size))
-            for code in sorted(status_counts.keys()):
-                if status_counts[code]:
-                    print("{}: {}".format(code, status_counts[code]))
+            counter += 1
+
+        if counter == 10:
+            print_stats()
+            counter = 0
+
 except KeyboardInterrupt:
-    pass
+    print_stats()
+    raise
+
 finally:
-    print("File size: {}".format(total_size))
-    for code in sorted(status_counts.keys()):
-        if status_counts[code]:
-            print("{}: {}".format(code, status_counts[code]))
+    print_stats()
+    
