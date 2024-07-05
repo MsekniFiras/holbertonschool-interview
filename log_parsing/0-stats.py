0912f3866496b34dc8eb349f7c8fe9cdd389f9e8
MsekniFiras <msekni.firas@gmail.com>
2024-07-05 19:51:35 +0100

    test

--- log_parsing/0-stats.py
+++ log_parsing/0-stats.py
@@ -1,30 +1,38 @@
 #!/usr/bin/python3
-""" Log parsing """
+"""
+Script that reads stdin line by line and computes metrics
+"""
 import sys
 
+total_size = 0
+counter = 0
 
-i = 0
-FileSize = 0
-STATUS = {'200': 0, '301': 0,
-          '400': 0, '401': 0,
-          '403': 0, '404': 0,
-          '405': 0, '500': 0}
+dict_codes_counter = {'200': 0, '301': 0, '400': 0, '401': 0,
+                      '403': 0, '404': 0, '405': 0, '500': 0}
+
 try:
     for line in sys.stdin:
-        i += 1
-        sp = line.split(' ')
-        if len(sp) > 2:
-            FileSize += int(sp[-1])
-            if sp[-2] in STATUS:
-                STATUS[sp[-2]] += 1
-        if i % 10 == 0:
-            print("File size: {}".format(FileSize))
-            for key, value in sorted(STATUS.items()):
-                if value != 0:
-                    print("{}: {}".format(key, value))
+        line_list = line.split(" ")
+        if len(line_list) > 2:
+            code = line_list[-2]
+            size = int(line_list[-1])
+            if code in dict_codes_counter.keys():
+                dict_codes_counter[code] += 1
+            total_size += size
+            counter += 1
+
+        if counter == 10:
+            print("File size: {:d}".format(total_size))
+            for k, v in sorted(dict_codes_counter.items()):
+                if v != 0:
+                    print("{}: {:d}".format(k, v))
+            counter = 0
+
+except Exception:
+    pass
+
 finally:
-    print("File size: {}".format(FileSize))
-    for key, value in sorted(STATUS.items()):
-        if value != 0:
-            print("{}: {:d}".format(key, value))
-            
+    print("File size: {}".format(total_size))
+    for k, v in sorted(dict_codes_counter.items()):
+        if v != 0:
+            print("{}: {}".format(k, v))
