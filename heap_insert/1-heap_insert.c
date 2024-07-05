d6699adf6e80b611e478f13ceef457b54b67438f
MsekniFiras <msekni.firas@gmail.com>
2024-07-05 17:04:29 +0100

    test

--- heap_insert/1-heap_insert.c
+++ heap_insert/1-heap_insert.c
@@ -15,32 +15,32 @@ void heapify_up(heap_t *node);
  */
 heap_t *heap_insert(heap_t **root, int value)
 {
-    heap_t *new_node;
+	heap_t *new_node;
 
-    /* Create the new node */
-    new_node = binary_tree_node(NULL, value);
-    if (new_node == NULL)
-        return (NULL);
+	/* Create the new node */
+	new_node = binary_tree_node(NULL, value);
+	if (new_node == NULL)
+		return (NULL);
 
-    /* Insert the new node at the end of the heap */
-    if (*root == NULL)
-    {
-        /* If the heap is empty, new node becomes the root */
-        *root = new_node;
-    }
-    else
-    {
-        /* Insert new node maintaining Max Heap property */
-        heap_t *insert_position = find_insert_position(*root);
-        if (insert_position->left == NULL)
-            insert_position->left = new_node;
-        else
-            insert_position->right = new_node;
-        new_node->parent = insert_position;
-        heapify_up(new_node);
-    }
+	/* Insert the new node at the end of the heap */
+	if (*root == NULL)
+	{
+		/* If the heap is empty, new node becomes the root */
+		*root = new_node;
+	}
+	else
+	{
+		/* Insert new node maintaining Max Heap property */
+		heap_t *insert_position = find_insert_position(*root);
+		if (insert_position->left == NULL)
+			insert_position->left = new_node;
+		else
+			insert_position->right = new_node;
+		new_node->parent = insert_position;
+		heapify_up(new_node);
+	}
 
-    return (new_node);
+	return (new_node);
 }
 
 /**
@@ -51,45 +51,45 @@ heap_t *heap_insert(heap_t **root, int value)
  */
 heap_t *find_insert_position(heap_t *root)
 {
-    /* Perform a level-order traversal to find the insertion point */
-    heap_t *queue[100]; /* Assume heap has no more than 100 nodes */
-    int front = -1, rear = -1;
-    heap_t *temp = root;
+	/* Perform a level-order traversal to find the insertion point */
+	heap_t *queue[100]; /* Assume heap has no more than 100 nodes */
+	int front = -1, rear = -1;
+	heap_t *temp = root;
 
-    while (temp != NULL)
-    {
-        if (front == -1)
-            front = rear = 0;
-        else
-            rear++;
-        queue[rear] = temp;
+	while (temp != NULL)
+	{
+		if (front == -1)
+			front = rear = 0;
+		else
+			rear++;
+		queue[rear] = temp;
 
-        /* Dequeue */
-        temp = queue[front];
-        front++;
+		/* Dequeue */
+		temp = queue[front];
+		front++;
 
-        /* If left child is NULL, insert here */
-        if (temp->left == NULL)
-            return (temp);
-        /* Otherwise, enqueue left child */
-        else
-        {
-            rear++;
-            queue[rear] = temp->left;
-        }
+		/* If left child is NULL, insert here */
+		if (temp->left == NULL)
+			return (temp);
+		/* Otherwise, enqueue left child */
+		else
+		{
+			rear++;
+			queue[rear] = temp->left;
+		}
 
-        /* If right child is NULL, insert here */
-        if (temp->right == NULL)
-            return (temp);
-        /* Otherwise, enqueue right child */
-        else
-        {
-            rear++;
-            queue[rear] = temp->right;
-        }
-    }
+		/* If right child is NULL, insert here */
+		if (temp->right == NULL)
+			return (temp);
+		/* Otherwise, enqueue right child */
+		else
+		{
+			rear++;
+			queue[rear] = temp->right;
+		}
+	}
 
-    return (NULL); /* This case should not happen */
+	return (NULL); /* This case should not happen */
 }
 
 /**
@@ -98,15 +98,15 @@ heap_t *find_insert_position(heap_t *root)
  */
 void heapify_up(heap_t *node)
 {
-    int temp;
-    heap_t *parent;
+	int temp;
+	heap_t *parent;
 
-    while (node->parent != NULL && node->n > node->parent->n)
-    {
-        parent = node->parent;
-        temp = node->n;
-        node->n = parent->n;
-        parent->n = temp;
-        node = parent;
-    }
+	while (node->parent != NULL && node->n > node->parent->n)
+	{
+		parent = node->parent;
+		temp = node->n;
+		node->n = parent->n;
+		parent->n = temp;
+		node = parent;
+	}
 }
