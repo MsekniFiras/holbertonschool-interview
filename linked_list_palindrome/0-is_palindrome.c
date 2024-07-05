d0d0a81ac915847520e88b73b701d46ff90a3eac
MsekniFiras <msekni.firas@gmail.com>
2024-07-05 19:15:38 +0100

    test

--- linked_list_palindrome/0-is_palindrome.c
+++ linked_list_palindrome/0-is_palindrome.c
@@ -1,37 +1,38 @@
 #include "lists.h"
 #include <stdio.h>
+
 /**
  * is_palindrome - checks if a singly linked list is a palindrome.
- * @head: double linked list to check.
- * Return: 1 if it's palindrome otherwise 0.
+ * @head: double pointer to the head of the linked list to check.
+ * Return: 1 if it's a palindrome otherwise 0.
  **/
-int is_palindrome(listint_t **head) {
-    if (!head || !*head || !(*head)->next) return 1;
+int is_palindrome(listint_t **head)
+{
+    listint_t *oldHead = *head; // Pointer to traverse the list
+    int counter = 0, i = 0; // Counters for the loop
+    int list[2048]; // Array to store the values of the list nodes
 
-    listint_t *slow = *head, *fast = *head, *prev = NULL, *temp = NULL;
+    // First loop: count the number of elements in the list
+    for (i = 0; oldHead; i++)
+        oldHead = oldHead->next;
+    // If the list is empty or has one element, it's a palindrome
+    if (i == 0)
+        return (1);
 
-    // Find the middle of the list
-    while (fast && fast->next) {
-        fast = fast->next->next;
-        // Reverse the first half while finding the middle
-        temp = slow->next;
-        slow->next = prev;
-        prev = slow;
-        slow = temp;
+    // Reset oldHead to the start of the list
+    oldHead = *head;
+    // Second loop: populate the array with the list's values
+    for (counter = 0; oldHead; oldHead = oldHead->next, counter++)
+        list[counter] = oldHead->n;
+
+    // Third loop: check if the list is a palindrome
+    for (counter = 0; counter < i; counter++)
+    {
+        // Compare the values from the start and end towards the center
+        if (list[counter] != list[i - counter - 1])
+            return (0); // Not a palindrome if any pair of values don't match
     }
 
-    // Handle odd number of elements
-    listint_t *second_half = (fast ? slow->next : slow);
-
-    // Compare the first half and the second half
-    while (prev && second_half) {
-        if (prev->n != second_half->n) {
-            return 0;
-        }
-        prev = prev->next;
-        second_half = second_half->next;
-    }
-
-
-    return 1;
+    // If all pairs of values matched, it's a palindrome
+    return (1);
 }
