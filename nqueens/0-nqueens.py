81b164462e743b1fe88cdae8f674cb17c5251a51
MsekniFiras <msekni.firas@gmail.com>
2024-07-06 18:18:26 +0100

    updated

--- nqueens/0-nqueens.py
+++ nqueens/0-nqueens.py
@@ -1,70 +1,56 @@
 #!/usr/bin/env python3
 #!/usr/bin/python3
-"""N queens classical problem."""
-import sys
+'''
+N queens
+'''
+
+from sys import argv
 
 
-def get_sol(board, n):
-    """Get solution from board."""
-    sols = []
-    for x in range(n):
-        for y in range(n):
-            if board[x][y]:
-                sols.append([x, y])
-    return sols
-
-
-def isvalid(board, pos, n):
-    """Check if position is valid on the board."""
-    for i in range(0, n):
-        if board[pos[0]][i]:
+def is_NQueen(cell: list) -> bool:
+    '''
+    True if N Queen, False if not
+    '''
+    row_number = len(cell) - 1
+    difference = 0
+    for index in range(0, row_number):
+        difference = cell[index] - cell[row_number]
+        if difference < 0:
+            difference *= -1
+        if difference == 0 or difference == row_number - index:
             return False
-        if board[i][pos[1]]:
-            return False
-        if pos[0]+i < n and pos[1]+i < n:
-            if board[pos[0]+i][pos[1]+i]:
-                return False
-        if pos[0]-i >= 0 and pos[1]-i >= 0:
-            if board[pos[0]-i][pos[1]-i]:
-                return False
-        if pos[0]+i < n and pos[1]-i >= 0:
-            if board[pos[0]+i][pos[1]-i]:
-                return False
-        if pos[0]-i >= 0 and pos[1]+i < n:
-            if board[pos[0]-i][pos[1]+i]:
-                return False
     return True
 
 
-def solvenqueen(board, col, n):
-    """Solve n queen using backtracking."""
-    if col >= n:
-        print(get_sol(board, n))
-        return
-    for i in range(n):
-        if isvalid(board, (i, col), n):
-            board[i][col] = True
-            solvenqueen(board, col+1, n)
-            board[i][col] = False
+def solve_NQeens(dimension: int, row: int, cell: list, outcome: list):
+    """
+    Return result of N Queens recusrivley
+    """
+    # Base case
+    if row == dimension:
+        print(outcome)
+    else:
+        for col in range(0, dimension):
+            cell.append(col)
+            outcome.append([row, col])
+            if (is_NQueen(cell)):
+                solve_NQeens(dimension, row + 1, cell, outcome)
+            cell.pop()
+            outcome.pop()
 
 
-def main():
-    """Solve n queen using backtracking."""
-    if len(sys.argv) == 1:
-        print("Usage: nqueens N")
-        exit(1)
-    try:
-        N = int(sys.argv[1])
-    except ValueError:
-        print("N must be a number")
-        exit(1)
-
-    if N < 4:
-        print('N must be at least 4')
-        exit(1)
-    board = [[False for _ in range(N)] for _ in range(N)]
-
-    solvenqueen(board, 0, N)
-
-
-main()
+if len(argv) != 2:
+    print('Usage: nqueens N')
+    exit(1)
+try:
+    N = int(argv[1])
+except BaseException:
+    print('N must be a number')
+    exit(1)
+if N < 4:
+    print('N must be at least 4')
+    exit(1)
+else:
+    outcome = []
+    cell = 0
+    solve_NQeens(int(N), cell, [], outcome)
