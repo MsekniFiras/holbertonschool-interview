21351039079beb5098c4811df9f4a69bbe58d5fb
MsekniFiras <msekni.firas@gmail.com>
2024-07-07 19:52:03 +0100

    rotate-2d-mat

--- rotate_2d_matrix/0-rotate_2d_matrix.py
+++ rotate_2d_matrix/0-rotate_2d_matrix.py
@@ -5,9 +5,10 @@ rotate 2D matrix module
 
 
 def rotate_2d_matrix(matrix):
-	n = len(matrix)
-	for i in range(n):
-		for j in range(i, n):
-			matrix[i][j], matrix[j][i] = matrix[j][i], matrix[i][j]
-	for i in range(n):
-		matrix[i].reverse()
+    n = len(matrix)
+    for i in range(n):
+        for j in range(i, n):
+            matrix[i][j], matrix[j][i] = matrix[j][i], matrix[i][j]
+    for i in range(n):
+        matrix[i].reverse()
+		
