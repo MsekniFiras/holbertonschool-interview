7bf596118554f47b476cb9a3edf9cf479b790cfa
MsekniFiras <msekni.firas@gmail.com>
2024-07-21 18:31:33 +0100

    makingchange

--- making_change/0-making_change.py
+++ making_change/0-making_change.py
@@ -1,23 +1,25 @@
 #!/usr/bin/python3
 """
-list calculation
+makeChange method
 """
 
 
 def makeChange(coins, total):
     """
-    make change: bottom up method
+    determine the fewest number of coins needed
+    to meet a given amount total
     """
+    number_coins = 0
+    cents = 0
     if total <= 0:
         return 0
-    if total in coins:
-        return 1
-    if len(coins) <= 0:
-        return -1
-    dp = [total + 1] * (total + 1)
-    dp[0] = 0
-    for a in range(1, total + 1):
-        for c in coins:
-            if a - c >= 0:
-                dp[a] = min(dp[a], 1 + dp[a - c])
-    return dp[total] if dp[total] != total + 1 else -1
+
+    coins = sorted(coins, reverse=True)
+
+    for coin in coins:
+        while cents + coin <= total:
+            cents += coin
+            number_coins += 1
+        if cents == total:
+            return number_coins
+    return -1
